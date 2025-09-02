a23bdad1b32441f5be1e87ffa69974c60e852901
Chris Searcy <csearcy@ball.com>
2025-09-01 21:35:51 -0600

    fixed some stuff, broke some stuff

---
+++ Models/RaiderIoApiModels/DefeatedEncounter.cs
@@ -0,0 +1,9 @@
+namespace Singularity.Models.RaiderIoApiModels
+{
+    public class DefeatedEncounter
+    {         
+        public string? Slug { get; set; }
+        public DateTime? LastDefeated { get; set; }
+        public DateTime? FirstDefeated { get; set; }        
+    }
+}
--- Models/RaiderIoApiModels/PulledEncounter.cs
+++ Models/RaiderIoApiModels/PulledEncounter.cs
@@ -1,11 +1,8 @@
 namespace Singularity.Models.RaiderIoApiModels
 {
-    public class Encounter
+    public class PulledEncounter
     {         
         public string? Slug { get; set; }
-        public DateTime? LastDefeated { get; set; }
-        public DateTime? FirstDefeated { get; set; }
-
         public int? Id { get; set; }
         public double? BestPercent { get; set; }
         public bool? IsDefeated { get; set; }
